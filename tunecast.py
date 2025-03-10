d5829c0254656081482097815db27f62f1e5a55f
Yash Shah <153555338+SaltySalsa12@users.noreply.github.com>
2025-03-10 16:16:07 -0600

    Update tunecast.py

--- tunecast.py
+++ tunecast.py
@@ -36,7 +36,6 @@ intents.message_content = True  # Changed to False as this is a privileged inten
 
 # FFmpeg options for processing audio
 ffmpeg_options = {
-    'executable': 'ffmpeg',
     'before_options':
     '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn'
